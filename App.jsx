1a7a5d606bcfa328c69739c635f945a13cb50bf5
ukatzi <ytalkov@gmail.com>
2025-10-16 00:00:18 +0500

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
@@ -7,19 +7,62 @@ import TopPlate from './topPlate/TopPlate'
 function App() {
   let [count, setCount] = useState(0);
   let [maxCount, setMaxCount] = useState(0);
-  let [ch1, setCh1] = useState(getCharcter());
-  let [ch2, setCh2] = useState(getCharcter());
-  function getCharcter(){
-    let character = fetch('https://api.rule34.gg/getRandomPosts?amount=1&tags=solo')
-      .then(response => response.json());
-    return character;
-  }
-  async function setCharacters(){
-    setCh1(await getCharcter());
-    setCh2(await getCharcter());
-  }
+  let [ch1, setCh1] = useState(0);
+  let [ch2, setCh2] = useState(0);
+  let refName1 = useRef(null), refCount1 = useRef(null), refSrc1 = useRef(null), refOnClick1 = useRef(null);
+  let refName2 = useRef(null), refCount2 = useRef(null), refSrc2 = useRef(null), refOnClick2 = useRef(null);
+  useEffect(() => {
+    let ignore = false;
+    let first_tag = Math.floor(Math.random() * 100);
+    let second_tag = Math.floor(Math.random() * 100);
+    //let first_tag = 0;
+    //let second_tag = 1;
+
+    //alert(first_tag);
+    let name_tags = fetch("https://api.rule34.gg/getTags?raw=true&limit=1000&type=is_character")
+      .then(data => data.json());
+
+    if(!ignore){  
+      let tag_promise1 = name_tags.then(data => data["tags"][first_tag]["tag_name"]);
+      let tag_promise2 = name_tags.then(data => data["tags"][second_tag]["tag_name"])
+
+      tag_promise1.then(data => {refName1.current.innerHTML = data;});
+      tag_promise2.then(data => {refName2.current.innerHTML = data;});
+
+      tag_promise1.then(data => {return fetch("https://api.rule34.gg/getRandomPosts?amount=1&tags=solo&tags=" + data)})
+        .then(data => data.json())
+        .then(data => {refSrc1.current.src = data["posts"][0]["preview"]["file"]});
+      tag_promise2.then(data => {return fetch("https://api.rule34.gg/getRandomPosts?amount=1&tags=solo&tags=" + data)})
+        .then(data => data.json())
+        .then(data => {refSrc2.current.src = data["posts"][0]["preview"]["file"]});
+    
+      let count_promise1 = name_tags.then(data => data["tags"][first_tag]["count"]);
+      let count_promise2 = name_tags.then(data => data["tags"][second_tag]["count"]);
+    
+      count_promise1.then(data => {refCount1.current.innerHTML = data});
+      count_promise2.then(data => {refCount2.current.innerHTML = data});
+
+      count_promise1.then(count1 => {
+        count_promise2.then(count2 => {
+          if(count1 > count2){
+            refOnClick1.current.onclick = onCorrect;
+            refOnClick2.current.onclick = onIncorrect;
+          }
+          else if(count1 == count2){
+            refOnClick1.current.onclick = onCorrect;
+            refOnClick2.current.onclick = onCorrect;
+          }
+          else{
+            refOnClick1.current.onclick = onIncorrect;
+            refOnClick2.current.onclick = onCorrect;
+          }
+        })
+      });
+    }
+    return (() => {ignore = true;});
+  });
   function onCorrect(){
-    setCount(count + 1);
+    setCount(count + 1); 
     setMaxCount(Math.max(maxCount, count + 1));
   }
   function onIncorrect(){
@@ -34,25 +77,23 @@ function App() {
         Record: {maxCount}
       </div>
       <div>
-        <Option onClick ={onCorrect} character = {ch1}></Option>
-        <Option onClick ={onIncorrect} character = {ch2}></Option> 
+        <Option refName = {refName1} refCount = {refCount1} refSrc = {refSrc1} refOnClick = {refOnClick1} character = {ch1}></Option>
+        <Option refName = {refName2} refCount = {refCount2} refSrc = {refSrc2} refOnClick = {refOnClick2} character = {ch2}></Option> 
       </div>
     </>
   )
 }
 
-function Option({onClick, character}) {
-  character.name = 0;
+function Option({refName, refCount, refSrc, refOnClick, character}) {
   return (
-  <button onClick={onClick}>
-    <ul>
-      {Object.keys(character).map(
-        element => (<li> {element}</li>)
-      )}
-    </ul>
-    <h1>
-      {typeof character}
-    </h1>
+  <button ref={refOnClick}>
+    <img ref={refSrc}></img>
+    <div ref={refName}>
+
+    </div>
+    <div ref={refCount}>
+
+    </div>
   </button>
   )
 }
